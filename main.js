7e682beb7c9253fad61449a725fe0559da719442
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-02-27 18:27:17 +0000

    Sample code

---
+++ main.js
@@ -0,0 +1,63 @@
+
+//TASK 1
+// const arr1 = [1, 2, 3];
+
+// const arrCopy = arr1.slice();
+// console.log(arrCopy);
+
+// const arr2 = [1,2,3,[10,20]];
+
+// const arrCopy2 = arr2.slice();
+// console.log(arrCopy2);
+
+
+//TASK 2
+
+// const arr1 = [1,2,3];
+
+// const arrToString = arr1.join();
+
+// console.log(arr1);
+// console.log(arrToString);
+
+
+// const arr2 = [10,200,3333];
+
+// const arrToString2 = arr2.join();
+
+// console.log(arr2);
+// console.log(arrToString2);
+
+
+//TASK 3
+
+// const arr1 = ['Ivan','Pavlo', 'Ira'];
+
+// let splice = arr1.splice(0, 1);
+// for (let i = 0; i < splice.length; i++) {
+//     console.log(splice.length);
+// };
+
+
+// let splice2 = arr1.splice(1, 1);
+// for (let i = 0; i < splice2.length; i++) {
+//     console.log(splice2.length)
+// };
+
+
+// let splice3 = arr1.splice(2, 1);
+// for (let i = 0; i < splice3.length; i++) {
+//     console.log(splice3.length)
+// };
+
+
+
+// const arr1 = arrCopy([1, 2, 3]);
+// function arrCopy(arr) {
+//     const a = arr.slice(0);
+//     return a;
+// }
+
+// console.log(arr1)
+
+
